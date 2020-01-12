1f92b78af7ab5db62dbf2eb21e27d5ba85199cbc
Timothy Adamcik <tadamcik0425@gmail.com>
2020-01-12 14:49:56 -0500

    Initial Commit

---
+++ onTheMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  onTheMap
+//
+//  Created by Timothy Adamcik on 1/12/20.
+//  Copyright © 2020 Timothy Adamcik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
