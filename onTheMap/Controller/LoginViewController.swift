de15b97f0c01ce43d86b31f19bd15780594b5a7c
Timothy Adamcik <tadamcik0425@gmail.com>
2020-01-12 19:58:52 -0500

    login view controller connected and formatted

---
+++ onTheMap/Controller/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginViewController.swift
+//  onTheMap
+//
+//  Created by Timothy Adamcik on 1/12/20.
+//  Copyright © 2020 Timothy Adamcik. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func loginButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func signUpButtonPressed(_ sender: Any) {
+    }
+}
+
---
+++ onTheMap/Model/OnTheMapClient/OTMClient.swift
@@ -0,0 +1,9 @@
+//
+//  OTMClient.swift
+//  onTheMap
+//
+//  Created by Timothy Adamcik on 1/12/20.
+//  Copyright © 2020 Timothy Adamcik. All rights reserved.
+//
+
+import Foundation
