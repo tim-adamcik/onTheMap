9ea656aef25311ee34b46b650b6d4c8aac5ffae9
Timothy Adamcik <tadamcik0425@gmail.com>
2020-02-04 17:45:16 -0500

    search feature not complete

--- onTheMap/Controller/AddLocationViewController.swift
+++ onTheMap/Controller/AddLocationViewController.swift
@@ -16,17 +16,20 @@ class AddLocationViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
     
     let locationManager = CLLocationManager()
+    let searchController = UISearchController(searchResultsController: nil)
     
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchController.searchBar.delegate = self
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
         locationManager.requestLocation()
         
-        
     }
+    
     
     @IBAction func dropPinLocation(_ sender: Any) {
     }
@@ -37,6 +40,7 @@ class AddLocationViewController: UIViewController {
 
 extension AddLocationViewController: UISearchBarDelegate, CLLocationManagerDelegate {
     
+    
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         if status == .authorizedWhenInUse {
             locationManager.requestLocation()
@@ -45,7 +49,7 @@ extension AddLocationViewController: UISearchBarDelegate, CLLocationManagerDeleg
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.first {
-            let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+            let span = MKCoordinateSpan(latitudeDelta: 0.9, longitudeDelta: 0.9)
             let region = MKCoordinateRegion(center: location.coordinate, span: span)
             mapView.setRegion(region, animated: true)
         }
@@ -55,31 +59,68 @@ extension AddLocationViewController: UISearchBarDelegate, CLLocationManagerDeleg
         print("Error: \(error)")
     }
     
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+         guard let mapView = mapView,
+                   let searchBarText = searchController.searchBar.text else { return }
+               let searchRequest = MKLocalSearch.Request()
+               searchRequest.naturalLanguageQuery = searchBarText
+               searchRequest.region = mapView.region
+               let search = MKLocalSearch(request: searchRequest)
+               search.start { (response, error) in
+                   if response == nil {
+                       print("Error: \(error?.localizedDescription ?? "Unknown Error").")
+                   } else {
+                       let annotations = self.mapView.annotations
+                       self.mapView.removeAnnotations(annotations)
+                       
+                       if let lat = response?.boundingRegion.center.latitude,
+                           let lon = response?.boundingRegion.center.longitude {
+                           
+                           let annotation = MKPointAnnotation()
+                           annotation.title = searchBarText
+                           annotation.coordinate = CLLocationCoordinate2DMake(lat, lon)
+                           self.mapView.addAnnotation(annotation)
+                           
+                           let coordinate = CLLocationCoordinate2DMake(lat, lon)
+                           let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+                           let region = MKCoordinateRegion(center: coordinate, span: span)
+                           self.mapView.setRegion(region, animated: true)
+                           
+                       }
+                   }
+               }
+    }
     
-    func searchForLocation(searchController: UISearchController) {
-        guard let mapView = mapView,
-            let searchBarText = searchController.searchBar.text else { return }
-        let searchRequest = MKLocalSearch.Request()
-        searchRequest.naturalLanguageQuery = searchBarText
-        searchRequest.region = mapView.region
-        let search = MKLocalSearch.init(request: searchRequest)
-        search.start { (response, error) in
-            if response == nil {
-                print("Error: \(error?.localizedDescription ?? "Unknown Error").")
-            } else {
-                let annotations = self.mapView.annotations
-                self.mapView.removeAnnotations(annotations)
-                
-                if let lat = response?.boundingRegion.center.latitude,
-                    let lon = response?.boundingRegion.center.longitude {
-                    
-                    let annotation = MKPointAnnotation()
-                    annotation.title = searchBarText
-                    annotation.coordinate = CLLocationCoordinate2DMake(lat, lon)
-                    self.mapView.addAnnotation(annotation)
-                }
-            }
-        }
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+//        guard let mapView = mapView,
+//            let searchBarText = searchController.searchBar.text else { return }
+//        let searchRequest = MKLocalSearch.Request()
+//        searchRequest.naturalLanguageQuery = searchBarText
+//        searchRequest.region = mapView.region
+//        let search = MKLocalSearch(request: searchRequest)
+//        search.start { (response, error) in
+//            if response == nil {
+//                print("Error: \(error?.localizedDescription ?? "Unknown Error").")
+//            } else {
+//                let annotations = self.mapView.annotations
+//                self.mapView.removeAnnotations(annotations)
+//
+//                if let lat = response?.boundingRegion.center.latitude,
+//                    let lon = response?.boundingRegion.center.longitude {
+//
+//                    let annotation = MKPointAnnotation()
+//                    annotation.title = searchBarText
+//                    annotation.coordinate = CLLocationCoordinate2DMake(lat, lon)
+//                    self.mapView.addAnnotation(annotation)
+//
+//                    let coordinate = CLLocationCoordinate2DMake(lat, lon)
+//                    let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+//                    let region = MKCoordinateRegion(center: coordinate, span: span)
+//                    self.mapView.setRegion(region, animated: true)
+//
+//                }
+//            }
+//        }
     }
     
 }
