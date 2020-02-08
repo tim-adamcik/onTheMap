0cf71c1721d4d6a288de69d77989479543493859
Timothy Adamcik <tadamcik0425@gmail.com>
2020-02-07 22:49:52 -0500

    updated userinfo request w url

--- onTheMap/Model/OnTheMapClient/OTMClient.swift
+++ onTheMap/Model/OnTheMapClient/OTMClient.swift
@@ -144,7 +144,7 @@ enum Endpoints {
     }
     
     class func getUserInfo(completion: @escaping (Error?) -> Void) {
-        let request = URLRequest(url: URL(string: "https://onthemap-api.udacity.com/v1/users/3903878747")!)
+        let request = URLRequest(url: Endpoints.userInfo.url)
         let session = URLSession.shared
         let task = session.dataTask(with: request) { data, response, error in
           if error != nil { // Handle error...
